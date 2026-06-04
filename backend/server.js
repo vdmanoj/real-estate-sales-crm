844d4551845c54c27c9534a1b04725f2be8d543d
Manoj VD <vdmanoj17@gmail.com>
2026-06-04 19:40:22 +0530

    Updated MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
 app.use("/uploads", express.static("uploads"));
 
 // ROUTES
@@ -16,24 +17,26 @@ const customerRoutes = require("./routes/customerRoutes");
 app.use("/api/users", userRoutes);
 app.use("/api/customers", customerRoutes);
 
-// MONGODB CONNECTION
-mongoose.connect(
-  "mongodb://127.0.0.1:27017/salesDB"
-)
-.then(() => {
-  console.log("MongoDB Connected");
-})
-.catch((err) => {
-  console.log("MongoDB Error");
-  console.log(err);
-});
-
-// TEST
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
-// SERVER
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
+// MONGODB CONNECTION
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("❌ MongoDB Error");
+    console.log(err);
+  });
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log(`🚀 Server Running on Port ${PORT}`);
 });
